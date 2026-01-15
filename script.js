81b14e3e9f0970149d9b03aac2ebcac82e152ed5
developer-098 <amansingh8967373@gmail.com>
2026-01-15 07:41:41 +0530

    toggler-change

--- script.js
+++ script.js
@@ -26,13 +26,17 @@ toggleBtn.addEventListener("click", () => {
                 const inputs = document.querySelectorAll(
                     "#contactForm input, #contactForm textarea"
                 );
-                // const navparent = document.querySelector(".nav-parent");
-                // navparent.style.background="var(--bg)";
                 inputs.forEach(el => {
                     el.style.border = "1px solid black";
 
                 });
 
+                const navbarTogglerIcon = document.querySelector(".navbar-toggler-icon");
+                navbarTogglerIcon.style.backgroundImage = `
+                url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba(0,0,0,1)' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='m4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`;
+
+                const navbarToggler = document.querySelector(".navbar-toggler");
+                navbarToggler.style.border = "1px solid black";
             } else {
                 section.style.background = "linear-gradient(135deg, var(--bg) 0%, rgba(15, 11, 29, 0.9) 100%)";
                 const inputs = document.querySelectorAll(
